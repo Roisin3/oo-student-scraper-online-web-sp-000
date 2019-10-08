c409466c99730cadaeda5d52319b5563a924748e
Michael Lynch <michael@michaelrlynch.com>
2019-10-08 00:09:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,3 @@ require 'pry'
 
 class Scraper
 end
-require 'nokogiri'
-require 'open-uri'
-
- site = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
- doc = Nokogiri::HTML(open(site))
